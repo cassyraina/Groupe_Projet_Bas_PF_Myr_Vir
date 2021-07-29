d14515281b4b2a6f096867220d0b9bbec76fd04b
cassy raina <cassyraina@gmail.com>
2021-07-29 16:17:55 +0200

    en cours

--- Eighties_Auction_Page_Accueil/contact.js
+++ Eighties_Auction_Page_Accueil/contact.js
@@ -1,5 +1,53 @@
-if (confirm("valider!")) {
-    txt = "complet!";
-  } else {
-    txt = "You pressed Cancel!";
-  }
+function W3docs()                                    
+{ 
+    var name = document.forms["RegForm"]["Nom"];
+    var name = document.forms["RegForm"]["Prenom"];       
+    var email = document.forms["RegForm"]["Email"];    
+    var phone = document.forms["RegForm"]["Téléphone"];  
+    var what =  document.forms["RegForm"]["message"];  
+    
+    if (name.value == "")                                  
+    { 
+        alert("Mettez votre nom."); 
+        name.focus(); 
+        return false; 
+    }    
+    if (address.value == "")                               
+    { 
+        alert("Prenom"); 
+        address.focus(); 
+        return false; 
+    }        
+     
+    if (email.value == "")                                   
+    { 
+        alert("Mettez une adresse email valide."); 
+        email.focus(); 
+        return false; 
+    }    
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        alert("Mettez une adresse email valide."); 
+        email.focus(); 
+        return false; 
+    }    
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        alert("Mettez une adresse email valide."); 
+        email.focus(); 
+        return false; 
+    }    
+    if (phone.value == "")                           
+    { 
+        alert("Mettez votre numéro de téléphone."); 
+        phone.focus(); 
+        return false; 
+    }    
+    if (password.value == "")                        
+    { 
+        alert("Message"); 
+        password.focus(); 
+        return false; 
+    }    
+    
+}
